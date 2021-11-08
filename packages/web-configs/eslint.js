329e179d50c71599741a1f1638061f18865e9113
ewnd9 <ewnd9@users.noreply.github.com>
2021-11-08 17:22:16 +0300

    feat(web-configs): init (#1)

---
+++ packages/web-configs/eslint.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier',
+        'plugin:codeceptjs/recommended',
+    ],
+    plugins: ['react-hooks', 'import'],
+    rules: {
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'react/display-name': 'off',
+        'react/prop-types': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'import/order': [
+            'error',
+            { groups: [['external', 'internal']], 'newlines-between': 'always-and-inside-groups' },
+        ],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
